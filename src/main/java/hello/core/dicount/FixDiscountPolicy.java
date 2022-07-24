bd41c296dc3d6f8a5a27f85d31f855b5121bbb8f
june <ohtjqkd@gmail.com>
2022-07-24 18:39:22 +0900

    Spring container and Spring bean

--- src/main/java/hello/core/dicount/FixDiscountPolicy.java
+++ src/main/java/hello/core/dicount/FixDiscountPolicy.java
@@ -8,7 +8,7 @@ public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000; // 1000원 할인
 
     @Override
-    public int dicount(Member member, int price) {
+    public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
         } else {
